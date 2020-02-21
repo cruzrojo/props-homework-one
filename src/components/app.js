b854fb1b2e4ca4bed4e198391c3901008b8755ce
cruzrojo <burkerich@gmail.com>
2020-02-21 16:25:19 -0700

    added a new div with the new fine funtional.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Greeting } from './functional'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Skeleton</h2>
+        <h1>Burke Rich's Prop Homework</h1>
+        <h2>React Skeleton...click...clack...Booooo!!!</h2>
+        <Greeting/>
       </div>
     );
   }
---
+++ src/components/functional.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Greeting = props => {
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
